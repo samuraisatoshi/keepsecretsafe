e6569d6a21779fa6df00b8424ba755e907836803
Decentralized Apps <contato@juliofabio.com>
2024-09-03 09:51:12 -0300

    Add files via upload
    Initial Commit

---
+++ lib/src/exceptions.dart
@@ -0,0 +1,18 @@
+/// Base exception class for Secret Manager related errors.
+class SecretManagerException implements Exception {
+  final String message;
+  SecretManagerException(this.message);
+
+  @override
+  String toString() => 'SecretManagerException: $message';
+}
+
+/// Exception class for authentication errors.
+class AuthenticationException extends SecretManagerException {
+  AuthenticationException(String message) : super(message);
+}
+
+/// Exception class for cases where a secret is not found.
+class SecretNotFoundException extends SecretManagerException {
+  SecretNotFoundException(String message) : super(message);
+}
