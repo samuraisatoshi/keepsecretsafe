9fd996c73d1e77b665db06e7b685971242f0385a
Decentralized Apps <contato@juliofabio.com>
2024-09-03 10:00:03 -0300

    Add files via upload
    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:keepsecretsafe/keepsecretsafe.dart';
+
+import 'src/authentication_service.dart';
+import 'src/secret_manager.dart';
+
+void main() async {
+  final authenticationService = AuthenticationService(
+    keyFilePath: './key.json',
+  );
+
+  await authenticationService.init();
+
+  final secretManager = SecretManager(
+    authenticationService: authenticationService,
+  );
+
+  try {
+    final secrets = await secretManager.getSecrets(['api_key', 'db_password']);
+    print('API Key: ${secrets['api_key']}');
+    print('DB Password: ${secrets['db_password']}');
+  } catch (e) {
+    print('Failed to fetch secrets: $e');
+  }
+}
